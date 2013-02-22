df8de23b9197eeb2d884a3c9922410bcea03ada8
Phil Christensen <phil@bubblehouse.org>
2013-02-21 21:05:36 -0500

    started to sketch out controller

--- CocoaArtnet/ANController.h
+++ CocoaArtnet/ANController.h
@@ -8,6 +8,29 @@
 
 #import <Foundation/Foundation.h>
 
-@interface ANController : NSObject
+@interface ANController : NSObject {
+    NSString* interfaceAddress;
+    float beatsPerMinute;
+    int barLength;
+    float framesPerSecond;
+    float framesPerBeat;
+    
+    NSArray* latestFrame;
+    NSArray* generators;
+    
+    int beatClock;
+    int secondFrameClock;
+    int beatFrameClock;
+    
+}
+
+-(ANController*) initWithAddress: (NSString*) address andBPM:(float) bpm andBarLength:(int) beats andFPS: (float) fps;
+-(ANController*) initWithAddress: (NSString*) address andBPM:(float) bpm andBarLength:(int) beats;
+-(ANController*) initWithAddress: (NSString*) address andBPM:(float) bpm;
+
+-(NSDictionary*) getClock;
+-(void) iterate;
+-(void) addGenerator: (SEL) name onTarget: (id) target;
+-(void) sendFrame: (NSArray*) frame;
 
 @end
--- CocoaArtnet/ANController.m
+++ CocoaArtnet/ANController.m
@@ -10,4 +10,62 @@
 
 @implementation ANController
 
+-(ANController*) initWithAddress: (NSString*) address andBPM:(float) bpm andBarLength:(int) beats andFPS: (float) fps {
+    [self setupWithAddress:address andBPM:bpm andBarLength:beats andFPS:fps];
+    return self;
+}
+
+
+-(ANController*) initWithAddress: (NSString*) address andBPM:(float) bpm andBarLength:(int) beats {
+    [self setupWithAddress:address andBPM:bpm andBarLength:beats andFPS:40.0];
+    return self;
+}
+
+-(ANController*) initWithAddress: (NSString*) address andBPM:(float) bpm {
+    [self setupWithAddress:address andBPM:bpm andBarLength:4 andFPS:40.0];
+    return self;
+}
+
+-(void) setupWithAddress: (NSString*) address andBPM:(float) bpm andBarLength:(int) beats andFPS: (float) fps {
+    interfaceAddress = address;
+    beatsPerMinute = bpm;
+    barLength = beats;
+    framesPerSecond = fps;
+    framesPerBeat = (fps * 60) / bpm;
+}
+
+-(NSDictionary*) getClock{
+    return @{
+         @"beatClock": @(beatClock),
+         @"barLength": @(barLength),
+         @"secondFrameClock": @(secondFrameClock),
+         @"framesPerSecond": @(framesPerSecond),
+         @"beatFrameClock": @(beatFrameClock),
+         @"framesPerBeat": @(framesPerBeat),
+         @"latestFrame": latestFrame,
+         @"running": @YES,
+    };
+}
+
+-(void) iterate {
+    secondFrameClock = secondFrameClock < framesPerSecond - 1 ? secondFrameClock + 1 : 0;
+    beatFrameClock = beatFrameClock < framesPerBeat - 1 ? beatFrameClock + 1 : 0;
+    if(beatFrameClock < framesPerBeat - 1){
+        beatFrameClock += 1;
+    }
+    else{
+        beatFrameClock = 0;
+        beatClock = beatClock < barLength -1 ? beatClock + 1 : 0;
+    }
+}
+
+-(void) addGenerator: (SEL) name onTarget: (id) target {
+
+}
+
+-(void) sendFrame: (NSArray*) frame {
+
+}
+
+
 @end
